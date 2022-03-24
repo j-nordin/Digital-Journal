9b05468e729e6f43f1d412242d734e9f5ea40f6e
axelhertzberg <axelher@student.chalmers.se>
2022-03-24 18:17:59 +0100

    Checkbuttons in componentsfolder

--- androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/MainActivity.kt
+++ androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/MainActivity.kt
@@ -4,18 +4,14 @@ package com.EENX15_22_17.digital_journal.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Row
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.res.stringResource
-import com.EENX15_22_17.digital_journal.android.ui.currentpatients.CurrentPatientItem
-import com.EENX15_22_17.digital_journal.android.ui.currentpatients.CurrentPatientsList
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CurrentPatientsList().PatientsList()
+            //CurrentPatientsList().PatientsList()
+//            CheckButton(handleSelected = { println(it)})
+//            CheckButtonWithText(text = "this is a button text", handleSelected = { println(it)})
         }
     }
 }
---
+++ androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/ui/arrivalpage/Arrival.kt
@@ -0,0 +1,10 @@
+package com.EENX15_22_17.digital_journal.android.ui.arrivalpage
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ArrivalContent(
+
+) {
+    
+}
