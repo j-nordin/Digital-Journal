3933362d65a96d6f205978a0334adb5678c89b65
Jonas Nordin <jonnordi@student.chalmers.se>
2022-03-25 11:04:01 +0100

    Navigation with arguments

--- androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/MainActivity.kt
+++ androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/MainActivity.kt
@@ -42,50 +42,98 @@ class MainActivity : ComponentActivity() {
 }
 
 sealed class Screen(val route: String) {
-    object Overview: Screen(route ="overview")
-    /*TODO: should take a patientId as parameter*/
-    object PatientMeeting: Screen(route = "patientMeeting")
+    object Overview : Screen(route = "overview")
+    object PatientMeeting : Screen(
+        route = "patientMeeting/{meetingId}"
+    ) {
+        fun createRoute(meetingId: String) = "patientMeeting/$meetingId"
+    }
 }
 
 @Preview
 @Composable
 fun NavigationApp() {
     val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = Screen.Overview.route) {
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Overview.route
+    ) {
         addOverviewGraph(navController = navController)
         addPatientMeetingGraph(navController = navController)
     }
 }
 
-private fun NavGraphBuilder.addOverviewGraph(navController: NavController) {
+private fun NavGraphBuilder.addOverviewGraph(
+    navController: NavController
+) {
     composable(route = Screen.Overview.route) {
-        OverviewScreen(navToPatientMeeting = { navController.navigate(Screen.PatientMeeting.route) })
+        /*TODO: replace with real overview screen*/
+        OverviewScreen(
+            navToPatientMeeting = { id ->
+                navController.navigate(Screen.PatientMeeting.createRoute(id))
+            },
+            goBack = { navController.popBackStack() }
+        )
     }
 }
 
-private fun NavGraphBuilder.addPatientMeetingGraph(navController: NavController) {
-    composable(route = Screen.PatientMeeting.route) {
-        /*TODO patientmeeting screen should take a patient id as argument, the argument comes from the given route*/
-        PatientMeetingScreen(navToOverview = { navController.navigate(Screen.Overview.route) })
+private fun NavGraphBuilder.addPatientMeetingGraph(
+    navController: NavController
+) {
+    composable(route = Screen.PatientMeeting.route) { backStackEntry ->
+        val meetingId = backStackEntry.arguments?.getString("meetingId")
+        requireNotNull(meetingId) { "No meeting id provided" }
+        /*TODO: replace with real patient meeting screen*/
+        PatientMeetingScreen(
+            navToOverview = { navController.navigate(Screen.Overview.route) },
+            meetingId = meetingId
+        )
     }
 }
 
 @Preview
 @Composable
-fun OverviewScreen(@PreviewParameter(OverviewScreenProvider::class) navToPatientMeeting: () -> Unit) {
-    Column(modifier = Modifier.fillMaxWidth().padding(40.dp)) {
-        Button(onClick = navToPatientMeeting) {
-            Text("Click for PatientMeeting")
+fun OverviewScreen(
+    @PreviewParameter(OverviewScreenProvider::class) navToPatientMeeting: (String) -> Unit,
+    goBack: () -> Unit = {}
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(40.dp)
+    ) {
+        Button(
+            onClick = { navToPatientMeeting("1") }
+        ) {
+            Text("Click for PatientMeeting 1")
+        }
+        Button(
+            onClick = { navToPatientMeeting("2") }
+        ) {
+            Text("Click for PatientMeeting 2")
+        }
+        Button(
+            onClick = goBack
+        ) {
+            Text(text = "Go back")
         }
     }
 }
 
 @Preview
 @Composable
-fun PatientMeetingScreen(@PreviewParameter(OverviewScreenProvider::class) navToOverview: () -> Unit) {
-    Button(onClick = navToOverview) {
-        Text("Click for overview")
+fun PatientMeetingScreen(
+    @PreviewParameter(OverviewScreenProvider::class) navToOverview: () -> Unit,
+    meetingId: String = "0"
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(40.dp)
+    ) {
+        Text(text = "This is patient meeting with id: $meetingId")
+        Button(onClick = navToOverview) {
+            Text("Click for overview")
+        }
     }
 }
-
-
