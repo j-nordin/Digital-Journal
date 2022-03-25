1702f60ebf964042e53b904679a4293fefe6a84a
axelhertzberg <axelher@student.chalmers.se>
2022-03-25 15:25:35 +0100

    Textefields changes
    * Added digit keyboard layout on textfield component
    * Added RememberSavable state on the textfields, so if you reload the
      emulator the value will be saved.
    * In ArrivalViewModel i just removed unused type in arrivalStates
      variable

--- androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/ui/arrivalpage/ArrivalViewModel.kt
+++ androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/ui/arrivalpage/ArrivalViewModel.kt
@@ -1,17 +1,14 @@
 package com.EENX15_22_17.digital_journal.android.ui.arrivalpage
 
-import android.icu.text.DateIntervalFormat
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
 import java.util.*
 
-class ArrivalViewModel() : ViewModel() {
+class ArrivalViewModel : ViewModel() {
 
-    var arrivalStates by mutableStateOf<ArrivalDataClass>(
+    var arrivalStates by mutableStateOf(
         ArrivalDataClass(
             date = Date(),
             timestamp = "",
