4d9dcff250a461aed954477ee760e3ff3e58dcd2
Jonas Nordin <jonnordi@student.chalmers.se>
2022-03-25 11:27:40 +0100

    Cleanup

--- androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/MainActivity.kt
+++ androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/MainActivity.kt
@@ -3,33 +3,19 @@ package com.EENX15_22_17.digital_journal.android
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.compose.animation.core.Animatable
-import androidx.compose.animation.core.VectorConverter
-import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.awaitFirstDown
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.Modifier
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
-import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.PreviewParameter
-import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.compose.runtime.Composable
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import androidx.compose.foundation.layout.Row
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
