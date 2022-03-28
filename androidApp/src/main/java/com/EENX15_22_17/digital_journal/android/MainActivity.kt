f03a3d5901887f1f904f288ded1a846b07c886df
Jonas Nordin <jonnordi@student.chalmers.se>
2022-03-28 11:18:31 +0200

    Initial nested navigation implemented

--- androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/MainActivity.kt
+++ androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/MainActivity.kt
@@ -4,7 +4,6 @@ package com.EENX15_22_17.digital_journal.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.compose.material.Text
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.Modifier
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
@@ -17,6 +16,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
+import androidx.navigation.navigation
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,14 +29,22 @@ class MainActivity : ComponentActivity() {
 
 sealed class Screen(val route: String) {
     object Overview : Screen(route = "overview")
-    object PatientMeeting : Screen(
-        route = "patientMeeting/{meetingId}"
-    ) {
-        fun createRoute(meetingId: String) = "patientMeeting/$meetingId"
+    object PatientMeeting : Screen(route = "patientMeeting/{meetingId}") {
+        fun createRoute(meetingId : String) = "patientMeeting/$meetingId"
     }
 }
 
-@Preview
+sealed class PatientMeetingScreens(open val route: String) {
+    abstract class MeetingSubPage(val sub: String) : PatientMeetingScreens(sub) {
+        override val route = Screen.PatientMeeting.route + "/" + sub
+        fun createRoute(id : String) = Screen.PatientMeeting.createRoute(id) + "/" + sub
+    }
+
+    object MainScreen : MeetingSubPage("mainScreen")
+    object Arrival : MeetingSubPage("arrival")
+    object HazardAssessment : MeetingSubPage("hazardAssessment")
+}
+
 @Composable
 fun NavigationApp() {
     val navController = rememberNavController()
@@ -46,6 +54,34 @@ fun NavigationApp() {
     ) {
         addOverviewGraph(navController = navController)
         addPatientMeetingGraph(navController = navController)
+    }
+}
+
+private fun NavGraphBuilder.addPatientMeetingGraph(
+    navController: NavController
+) {
+    navigation(
+        route = Screen.PatientMeeting.route,
+        startDestination = PatientMeetingScreens.MainScreen.route
+    ) {
+        composable(route = PatientMeetingScreens.MainScreen.route) { backStackEntry ->
+            val meetingId = backStackEntry.arguments?.getString("meetingId")
+
+            /*TODO: Handle meeting id not provided, ex show alert*/
+            requireNotNull(meetingId) { "No meeting id provided" }
+
+            /*TODO: replace with real patient meeting screen*/
+            PatientMeetingScreen(
+                navToOverview = { navController.navigate(Screen.Overview.route) },
+                navToArrival = { navController.navigate(PatientMeetingScreens.Arrival.createRoute(meetingId)) },
+                meetingId = meetingId
+            )
+        }
+        composable(route = PatientMeetingScreens.Arrival.route) { backStackEntry ->
+            val meetingId = backStackEntry.arguments?.getString("meetingId")
+            requireNotNull(meetingId) { "No meeting id provided" }
+            ArrivalScreen(meetingId)
+        }
     }
 }
 
@@ -63,21 +99,20 @@ private fun NavGraphBuilder.addOverviewGraph(
     }
 }
 
-private fun NavGraphBuilder.addPatientMeetingGraph(
-    navController: NavController
-) {
-    composable(route = Screen.PatientMeeting.route) { backStackEntry ->
-        val meetingId = backStackEntry.arguments?.getString("meetingId")
-        requireNotNull(meetingId) { "No meeting id provided" }
-        /*TODO: replace with real patient meeting screen*/
-        PatientMeetingScreen(
-            navToOverview = { navController.navigate(Screen.Overview.route) },
-            meetingId = meetingId
-        )
+@Composable
+fun ArrivalScreen(meetingId: String) {
+    Column() {
+        Text(text = "This is the arrival screen for meetingid $meetingId")
     }
 }
 
-@Preview
+@Composable
+fun HazardScreen(meetingId: String) {
+    Column() {
+        Text(text = "This is the hazerd screen for meetingid $meetingId")
+    }
+}
+
 @Composable
 fun OverviewScreen(
     @PreviewParameter(OverviewScreenProvider::class) navToPatientMeeting: (String) -> Unit,
@@ -106,10 +141,11 @@ fun OverviewScreen(
     }
 }
 
-@Preview
+
 @Composable
 fun PatientMeetingScreen(
     @PreviewParameter(OverviewScreenProvider::class) navToOverview: () -> Unit,
+    navToArrival: () -> Unit,
     meetingId: String = "0"
 ) {
     Column(
@@ -120,6 +156,9 @@ fun PatientMeetingScreen(
         Text(text = "This is patient meeting with id: $meetingId")
         Button(onClick = navToOverview) {
             Text("Click for overview")
+        }
+        Button(onClick = navToArrival) {
+            Text("Go to arrival page for patient")
         }
     }
 }
