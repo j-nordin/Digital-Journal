aa10efe84a30bf042010bcbdf3d995ec2b08e047
erikbengtsson <eribeng@student.chalmers.se>
2022-03-28 17:21:35 +0200

    refactor hard coded cards and added modifierProvider to make card creation mor dynamic

--- androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/ui/landingpage/landingPage.kt
+++ androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/ui/landingpage/landingPage.kt
@@ -6,25 +6,46 @@ import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.EENX15_22_17.digital_journal.android.R
-import com.EENX15_22_17.digital_journal.android.ui.theme.arrivalCard
-import com.EENX15_22_17.digital_journal.android.ui.theme.cardColor2
+import com.EENX15_22_17.digital_journal.android.ui.theme.lineOneCard
+import com.EENX15_22_17.digital_journal.android.ui.theme.lineTwoCard
 
 class landingPage {
 
     enum class NavigationEnum {
-        ARRIVAL, DANGER, HEALTH
+        ARRIVAL, DANGER, CONTACT_REASON, PREVIOUS_CARE, HEALTH_HISTORY, HEALTH_NOW
+    }
+
+    object ModifierProvider {
+
+        fun lineOneModifier(
+            NavigationTarget: NavigationEnum,
+            navigateToForm: (nav: NavigationEnum) -> Unit = {}
+        ): Modifier {
+            return if (NavigationTarget == NavigationEnum.ARRIVAL || NavigationTarget == NavigationEnum.DANGER) {
+                Modifier
+                    .height(100.dp)
+                    .width(170.dp)
+                    .clickable { navigateToForm(NavigationTarget) }
+            } else if (NavigationTarget == NavigationEnum.CONTACT_REASON || NavigationTarget == NavigationEnum.PREVIOUS_CARE || NavigationTarget == NavigationEnum.HEALTH_HISTORY) {
+                return Modifier
+                    .width(110.dp)
+                    .height(100.dp)
+                    .clickable { navigateToForm(NavigationTarget) }
+            } else {
+                return Modifier.width(100.dp)
+            }
+        }
     }
 
     @Composable
     fun landingPage(
-        navigateToForm: (nav: NavigationEnum) -> Unit = {
-            println(1)
-        }
+        navigateToForm: (nav: NavigationEnum) -> Unit = {}
     ) {
         Column(
             modifier = Modifier,
@@ -34,11 +55,19 @@ class landingPage {
             InfoDisplay()
             Row(
                 modifier = Modifier
-                  .fillMaxWidth(),
+                    .fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceEvenly
             ) {
-                ArrivalCard(navigateToForm)
-                DangerCard(navigateToForm)
+                navigationCard(
+                    R.string.arrivalCard,
+                    lineOneCard,
+                    ModifierProvider.lineOneModifier(NavigationEnum.ARRIVAL, navigateToForm)
+                )
+                navigationCard(
+                    R.string.hazardCard,
+                    lineOneCard,
+                    ModifierProvider.lineOneModifier(NavigationEnum.DANGER, navigateToForm)
+                )
             }
             Row(
                 modifier = Modifier
@@ -47,9 +76,21 @@ class landingPage {
                 horizontalArrangement = Arrangement.SpaceEvenly
             )
             {
-                ArrivalCard(navigateToForm)
-                DangerCard(navigateToForm)
-                ArrivalCard(navigateToForm)
+                navigationCard(
+                    R.string.contactReason,
+                    lineTwoCard,
+                    ModifierProvider.lineOneModifier(NavigationEnum.CONTACT_REASON, navigateToForm)
+                )
+                navigationCard(
+                    R.string.previousCare,
+                    lineTwoCard,
+                    ModifierProvider.lineOneModifier(NavigationEnum.PREVIOUS_CARE, navigateToForm)
+                )
+                navigationCard(
+                    R.string.healthHistory,
+                    lineTwoCard,
+                    ModifierProvider.lineOneModifier(NavigationEnum.HEALTH_HISTORY, navigateToForm)
+                )
             }
             Row(
                 modifier = Modifier
@@ -58,8 +99,6 @@ class landingPage {
                 horizontalArrangement = Arrangement.SpaceEvenly
             )
             {
-                DangerCard(navigateToForm)
-                DangerCard(navigateToForm)
             }
         }
     }
@@ -75,8 +114,8 @@ class landingPage {
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(bottom = 5.dp, start = 5.dp, end = 5.dp, top = 5.dp)
-            ){
-                Row(){
+            ) {
+                Row() {
                     Text("Namn: Erik Bengtsson", textAlign = TextAlign.Start)
                     Text("Personnummer: 1997001122-XXXX", textAlign = TextAlign.End)
                 }
@@ -85,38 +124,16 @@ class landingPage {
     }
 
     @Composable
-    fun ArrivalCard(
-        navigateToForm: (nav: NavigationEnum) -> Unit
+    fun navigationCard(
+        label: Int,
+        backgroundCol: Color,
+        modifier: Modifier
     ) {
         Card(
-            modifier = Modifier
-                .height(100.dp)
-                .width(80.dp)
-                .clickable { navigateToForm(NavigationEnum.ARRIVAL) },
-            backgroundColor = arrivalCard
+            modifier = modifier,
+            backgroundColor = backgroundCol
         ) {
-            Text(
-                stringResource(R.string.arrivalCard),
-                fontWeight = FontWeight.Bold,
-                textAlign = TextAlign.Center,
-            )
+            Text(stringResource(label), fontWeight = FontWeight.Bold, textAlign = TextAlign.Center)
         }
     }
-
-    @Composable
-    fun DangerCard(
-        navigateToForm: (nav: NavigationEnum) -> Unit
-    ) {
-        Card(
-            modifier = Modifier
-                .height(100.dp)
-                .clickable { navigateToForm(NavigationEnum.DANGER) },
-            backgroundColor = cardColor2
-
-        ) {
-            Text(stringResource(R.string.hazardCard), fontWeight = FontWeight.Bold)
-        }
-    }
-
-
 }
