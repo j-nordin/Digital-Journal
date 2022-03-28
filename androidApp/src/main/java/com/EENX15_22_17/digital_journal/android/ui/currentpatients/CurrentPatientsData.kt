e88345a60f0b3ecc28fc2f40155f8b3c7759a833
Jonas Nordin <jonnordi@student.chalmers.se>
2022-03-28 14:45:07 +0200

    Removed unnecessary prefix

--- androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/ui/currentpatients/CurrentPatientsData.kt
+++ androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/ui/currentpatients/CurrentPatientsData.kt
@@ -2,9 +2,9 @@ package com.EENX15_22_17.digital_journal.android.ui.currentpatients
 
 // Temporary dataclass before backend is implemented
 data class CurrentPatientsData(
-    val patientName: String,
-    val patientId: String,
-    val patientSecurityNumber: String
+    val name: String,
+    val id: String,
+    val securityNumber: String
 )
 
 
