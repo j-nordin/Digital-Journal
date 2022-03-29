50323c9e5d1abf9c300a1a017199798be6880839
erikbengtsson <eribeng@student.chalmers.se>
2022-03-29 13:31:13 +0200

    started to implement a composable for overview button on bottom right of page

--- androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/MainActivity.kt
+++ androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/MainActivity.kt
@@ -10,14 +10,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import com.EENX15_22_17.digital_journal.android.ui.currentpatients.CurrentPatientItem
 import com.EENX15_22_17.digital_journal.android.ui.currentpatients.CurrentPatientsList
-import com.EENX15_22_17.digital_journal.android.ui.landingpage.landingPage
+import com.EENX15_22_17.digital_journal.android.ui.landingpage.LandingPage
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             // CurrentPatientsList().PatientsList()
-            landingPage().landingPage()
+            LandingPage().landingPage()
         }
     }
 }
