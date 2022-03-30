f988b656f552c8b201c56464081760fc983e8525
Jonas Nordin <jonnordi@student.chalmers.se>
2022-03-30 15:00:15 +0200

    Initial theme

--- androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/MainActivity.kt
+++ androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/MainActivity.kt
@@ -16,12 +16,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
 import androidx.navigation.*
 import com.EENX15_22_17.digital_journal.android.ui.currentpatients.PatientsList
+import com.EENX15_22_17.digital_journal.android.ui.theme.DigitalJournalTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            NavigationApp()
+            DigitalJournalTheme {
+                NavigationApp()
+            }
         }
     }
 }
