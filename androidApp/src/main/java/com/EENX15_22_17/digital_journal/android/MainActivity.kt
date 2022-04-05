e7aedf3ffed617a1c1327bc08d8db4f98b30729e
Jonas Nordin <jonnordi@student.chalmers.se>
2022-04-05 16:51:19 +0200

    Cleanup

--- androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/MainActivity.kt
+++ androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/MainActivity.kt
@@ -4,18 +4,9 @@ package com.EENX15_22_17.digital_journal.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.EENX15_22_17.digital_journal.android.ui.arrivalpage.ArrivalPage
 import androidx.compose.material.Text
-import androidx.compose.ui.Modifier
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
-import androidx.compose.ui.tooling.preview.PreviewParameter
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.unit.dp
 import com.EENX15_22_17.digital_journal.android.ui.theme.DigitalJournalTheme
 
 
@@ -30,33 +21,6 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun HazardScreen(visitId: String) {
-    Column() {
-        Text(text = "This is the hazerd screen for visitId $visitId")
-    }
-}
-
-@Composable
-fun PatientMeetingLandingScreen(
-    @PreviewParameter(OverviewScreenProvider::class) navToOverview: () -> Unit,
-    navToArrival: () -> Unit,
-    visitId: String = "0"
-) {
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(40.dp)
-    ) {
-        Text(text = "This is patient meeting with id: $visitId")
-        Button(onClick = navToOverview) {
-            Text("Click to get pack to current board")
-        }
-        Button(onClick = navToArrival) {
-            Text("Go to arrival page for patient")
-        }
-    }
-}
 
 @Composable
 fun PatientOverviewPage(
