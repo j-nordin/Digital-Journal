1ec10cca2cf7efa2963f2741cce81c52a4ccb1e0
erikbengtsson <eribeng@student.chalmers.se>
2022-04-05 14:51:58 +0200

    connected the navigation to landingPage

--- androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/AppNavigation.kt
+++ androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/AppNavigation.kt
@@ -1,5 +1,6 @@
 package com.EENX15_22_17.digital_journal.android
 
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
@@ -9,6 +10,8 @@ import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navigation
 import com.EENX15_22_17.digital_journal.android.ui.current.CurrentScreen
 import com.EENX15_22_17.digital_journal.android.ui.arrivalpage.ArrivalPage
+import com.EENX15_22_17.digital_journal.android.ui.landingpage.LandingPage
+import com.EENX15_22_17.digital_journal.android.ui.screen.ContactCauseScreen
 
 sealed class Screen(val route: String) {
     object Overview : Screen(route = "overview")
@@ -32,7 +35,37 @@ sealed class PatientMeetingScreens(val route: String) {
     }
 
     // TODO: Implement routes to rest of the cards
-    object HazardAssessment : PatientMeetingScreens("{visitId}/hazardAssessment")
+    object HazardAssessment : PatientMeetingScreens("{visitId}/hazardAssessment") {
+        fun createRoute(visitId: String, root: Screen) = "${root.route}/$visitId/hazardAssessment"
+    }
+
+    object ContactReason : PatientMeetingScreens("{visitId}/contactReason") {
+        fun createRoute(visitId: String, root: Screen) = "${root.route}/$visitId/contactReason"
+    }
+
+    object PreviousCare : PatientMeetingScreens("{visitId}/previousCare") {
+        fun createRoute(visitId: String, root: Screen) = "${root.route}/$visitId/previousCare"
+    }
+
+    object HealthHistory : PatientMeetingScreens("{visitId}/healthHistory") {
+        fun createRoute(visitId: String, root: Screen) = "${root.route}/$visitId/healthHistory"
+    }
+
+    object HealthNow : PatientMeetingScreens("{visitId}/healthNow") {
+        fun createRoute(visitId: String, root: Screen) = "${root.route}/$visitId/healthNow"
+    }
+    object SuicideAssessment : PatientMeetingScreens("{visitId}/suicideAssessment") {
+        fun createRoute(visitId: String, root: Screen) = "${root.route}/$visitId/suicideAssessment"
+    }
+    object NursingNeed : PatientMeetingScreens("{visitId}/nursingNeed") {
+        fun createRoute(visitId: String, root: Screen) = "${root.route}/$visitId/nursingNeed"
+    }
+    object MedicalOrder : PatientMeetingScreens("{visitId}/medicalOrder") {
+        fun createRoute(visitId: String, root: Screen) = "${root.route}/$visitId/medicalOrder"
+    }
+    object InterimJournal : PatientMeetingScreens("{visitId}/interimJournal") {
+        fun createRoute(visitId: String, root: Screen) = "${root.route}/$visitId/interimJournal"
+    }
 }
 
 
@@ -64,17 +97,68 @@ private fun NavGraphBuilder.addPatientMeetingGraph(
             requireNotNull(visitId) { "No meeting id provided" }
 
             /*TODO: replace with real patient meeting screen*/
-            PatientMeetingLandingScreen(
-                navToOverview = { navController.navigate(Screen.Overview.route) },
+
+            LandingPage(
                 navToArrival = {
-                    navController.navigate(
-                        PatientMeetingScreens.Arrival.createRoute(
-                            visitId = visitId,
-                            root = Screen.PatientMeeting
-                        )
+                    navToArrival(
+                        navController = navController,
+                        visitId = visitId
                     )
                 },
-                visitId = visitId
+                navToHazard = {
+                    navToHazardAssessment(
+                        navController = navController,
+                        visitId = visitId
+                    )
+                },
+                navToContactReason = {
+                    navToContactReason(
+                        navController = navController,
+                        visitId = visitId
+                    )
+                },
+                navToPreviousCare = {
+                    navToPreviousCare(
+                        navController = navController,
+                        visitId = visitId
+                    )
+                },
+                navToHealthHistory = {
+                    navToHealthHistory(
+                        navController = navController,
+                        visitId = visitId
+                    )
+                },
+                navToHealthNow = {
+                    navToHealthNow(
+                        navController = navController,
+                        visitId = visitId
+                    )
+                },
+                navToSuicideAssessment = {
+                    navToSuicideAssessment(
+                        navController = navController,
+                        visitId = visitId
+                    )
+                },
+                navToNursingNeed = {
+                    navToNursingNeed(
+                        navController = navController,
+                        visitId = visitId
+                    )
+                },
+                navToMedicalOrder = {
+                    navToMedicalOrder(
+                        navController = navController,
+                        visitId = visitId
+                    )
+                },
+                navToInterimJournal = {
+                    navToInterimJournal(
+                        navController = navController,
+                        visitId = visitId
+                    )
+                }
             )
         }
         composable(
@@ -87,7 +171,162 @@ private fun NavGraphBuilder.addPatientMeetingGraph(
                 navBack = { navController.popBackStack() }
             )
         }
+        composable(
+            route = PatientMeetingScreens.HazardAssessment.createRoute(Screen.PatientMeeting)
+        ) { backStackEntry ->
+            val visitId = backStackEntry.arguments?.getString("visitId")
+            requireNotNull(visitId) { "No patient id provided " }
+            //TODO add hazard composable here
+            //Text("hej")
+        }
+        composable(
+            route = PatientMeetingScreens.ContactReason.createRoute(Screen.PatientMeeting)
+        ) { backStackEntry ->
+            val visitId = backStackEntry.arguments?.getString("visitId")
+            requireNotNull(visitId) { "No patient meeting" }
+            // TODO add contact reason composable
+            ContactCauseScreen()
+
+        }
+        composable(
+            route = PatientMeetingScreens.PreviousCare.createRoute(Screen.PatientMeeting)
+        ) { backStackEntry ->
+            val visitId = backStackEntry.arguments?.getString("visitId")
+            requireNotNull(visitId) { "No patient meeting" }
+            // TODO add PreviousCare composable
+        }
+        composable(
+            route = PatientMeetingScreens.HealthHistory.createRoute(Screen.PatientMeeting)
+        ) { backStackEntry ->
+            val visitId = backStackEntry.arguments?.getString("visitId")
+            requireNotNull(visitId) { "No patient meeting" }
+            // TODO add HealthHistory composable
+        }
+        composable(
+            route = PatientMeetingScreens.HealthNow.createRoute(Screen.PatientMeeting)
+        ) { backStackEntry ->
+            val visitId = backStackEntry.arguments?.getString("visitId")
+            requireNotNull(visitId) { "No patient meeting" }
+            // TODO add HealthNow composable
+        }
+        composable(
+            route = PatientMeetingScreens.SuicideAssessment.createRoute(Screen.PatientMeeting)
+        ) { backStackEntry ->
+            val visitId = backStackEntry.arguments?.getString("visitId")
+            requireNotNull(visitId) { "No patient meeting" }
+            // TODO add SuicideAssessment composable
+        }
+        composable(
+            route = PatientMeetingScreens.NursingNeed.createRoute(Screen.PatientMeeting)
+        ) { backStackEntry ->
+            val visitId = backStackEntry.arguments?.getString("visitId")
+            requireNotNull(visitId) { "No patient meeting" }
+            // TODO add nursingNeed composable
+        }
+        composable(
+            route = PatientMeetingScreens.MedicalOrder.createRoute(Screen.PatientMeeting)
+        ) { backStackEntry ->
+            val visitId = backStackEntry.arguments?.getString("visitId")
+            requireNotNull(visitId) { "No patient meeting" }
+            // TODO add medicalOrder composable
+        }
+        composable(
+            route = PatientMeetingScreens.InterimJournal.createRoute(Screen.PatientMeeting)
+        ) { backStackEntry ->
+            val visitId = backStackEntry.arguments?.getString("visitId")
+            requireNotNull(visitId) { "No patient meeting" }
+            // TODO add interimJournal composable
+        }
     }
+}
+
+
+fun navToArrival(navController: NavController, visitId: String) {
+    navController.navigate(
+        PatientMeetingScreens.Arrival.createRoute(
+            visitId = visitId,
+            root = Screen.PatientMeeting
+        )
+    )
+}
+
+fun navToHazardAssessment(navController: NavController, visitId: String) {
+    navController.navigate(
+        PatientMeetingScreens.HazardAssessment.createRoute(
+            visitId = visitId,
+            root = Screen.PatientMeeting
+        )
+    )
+}
+
+fun navToContactReason(navController: NavController, visitId: String) {
+    navController.navigate(
+        PatientMeetingScreens.ContactReason.createRoute(
+            visitId = visitId,
+            root = Screen.PatientMeeting
+        )
+    )
+}
+
+fun navToPreviousCare(navController: NavController, visitId: String) {
+    navController.navigate(
+        PatientMeetingScreens.PreviousCare.createRoute(
+            visitId = visitId,
+            root = Screen.PatientMeeting
+        )
+    )
+}
+
+fun navToHealthHistory(navController: NavController, visitId: String) {
+    navController.navigate(
+        PatientMeetingScreens.HealthHistory.createRoute(
+            visitId = visitId,
+            root = Screen.PatientMeeting
+        )
+    )
+}
+
+fun navToHealthNow(navController: NavController, visitId: String) {
+    navController.navigate(
+        PatientMeetingScreens.HealthNow.createRoute(
+            visitId = visitId,
+            root = Screen.PatientMeeting
+        )
+    )
+}
+
+fun navToSuicideAssessment(navController: NavController,visitId: String){
+    navController.navigate(
+        PatientMeetingScreens.SuicideAssessment.createRoute(
+            visitId = visitId,
+            root = Screen.PatientMeeting
+        )
+    )
+}
+fun navToNursingNeed(navController: NavController,visitId: String){
+    navController.navigate(
+        PatientMeetingScreens.NursingNeed.createRoute(
+            visitId = visitId,
+            root = Screen.PatientMeeting
+        )
+    )
+}
+fun navToMedicalOrder(navController: NavController,visitId: String){
+    navController.navigate(
+        PatientMeetingScreens.MedicalOrder.createRoute(
+            visitId = visitId,
+            root = Screen.PatientMeeting
+        )
+    )
+}
+
+fun navToInterimJournal(navController: NavController,visitId: String){
+    navController.navigate(
+        PatientMeetingScreens.InterimJournal.createRoute(
+            visitId = visitId,
+            root = Screen.PatientMeeting
+        )
+    )
 }
 
 
--- androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/ui/landingpage/landingPage.kt
+++ androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/ui/landingpage/landingPage.kt
@@ -24,208 +24,216 @@ import androidx.compose.ui.unit.sp
 import com.EENX15_22_17.digital_journal.android.ui.theme.colorIcon
 
 
-class LandingPage {
+@Composable
+fun LandingPage(
+    showOverview: (patientId: String) -> Unit = {},
+    navToArrival: () -> Unit = {},
+    navToHazard: () -> Unit = {},
+    navToContactReason: () -> Unit = {},
+    navToPreviousCare: () -> Unit = {},
+    navToHealthHistory: () -> Unit = {},
+    navToHealthNow: () -> Unit = {},
+    navToSuicideAssessment: () -> Unit = {},
+    navToNursingNeed: () -> Unit = {},
+    navToMedicalOrder: () -> Unit = {},
+    navToInterimJournal: () -> Unit = {}
 
-    /*
-    TODO: refactor these to own file
-     */
-    enum class NavigationEnum {
-        ARRIVAL, DANGER, CONTACT_REASON, PREVIOUS_CARE, HEALTH_HISTORY, HEALTH_NOW, SUICIDE_ASSESSMENT, NURSING_NEED, MEDICAL_ORDER, INTERIM_JOURNAL
-    }
-
-
-    @Composable
-    fun landingPage(
-        navigateToForm: (nav: NavigationEnum) -> Unit = {},
-        showOverview: (patientId: String) -> Unit = {}
-    ) {
-        Column(
-            modifier = Modifier,
-            verticalArrangement = Arrangement.Center
-        )
-        {
-            InfoDisplay()
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceEvenly
-            ) {
-                navigationCard(
-                    label = R.string.arrivalCard,
-                    backgroundCol = rettsLineOneNavigationCard,
-                    modifier = Modifier.width(370.dp),
-                    navTarget = NavigationEnum.ARRIVAL,
-                    navigateToForm = navigateToForm
-                )
-                navigationCard(
-                    label = R.string.hazardCard,
-                    backgroundCol = rettsLineOneNavigationCard,
-                    modifier = Modifier.width(370.dp),
-                    navTarget = NavigationEnum.DANGER,
-                    navigateToForm = navigateToForm
-                )
-            }
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 10.dp),
-                horizontalArrangement = Arrangement.SpaceEvenly
-            )
-            {
-                navigationCard(
-                    label = R.string.contactReason,
-                    backgroundCol = rettsLineTwoNavigationCard,
-                    modifier = Modifier
-                        .width(240.dp),
-                    navTarget = NavigationEnum.CONTACT_REASON,
-                    navigateToForm = navigateToForm
-                )
-                navigationCard(
-                    label = R.string.previousCare,
-                    backgroundCol = rettsLineTwoNavigationCard,
-                    modifier = Modifier
-                        .width(240.dp),
-                    navTarget = NavigationEnum.PREVIOUS_CARE,
-                    navigateToForm = navigateToForm
-                )
-                navigationCard(
-                    label = R.string.healthHistory,
-                    backgroundCol = rettsLineTwoNavigationCard,
-                    modifier = Modifier
-                        .width(240.dp),
-                    navTarget = NavigationEnum.HEALTH_HISTORY,
-                    navigateToForm = navigateToForm
-                )
-            }
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 10.dp),
-                horizontalArrangement = Arrangement.SpaceEvenly
-            )
-            {
-                navigationCard(
-                    label = R.string.healthNow,
-                    backgroundCol = rettsLineTwoNavigationCard,
-                    modifier = Modifier
-                        .width(370.dp),
-                    navTarget = NavigationEnum.HEALTH_NOW,
-                    navigateToForm = navigateToForm
-                )
-                navigationCard(
-                    label = R.string.suicideAssessment,
-                    backgroundCol = rettsLineTwoNavigationCard,
-                    modifier = Modifier
-                        .width(370.dp),
-                    navTarget = NavigationEnum.SUICIDE_ASSESSMENT,
-                    navigateToForm = navigateToForm
-                )
-            }
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 10.dp),
-                horizontalArrangement = Arrangement.SpaceEvenly
-            ) {
-                navigationCard(
-                    label = R.string.nursingNeed,
-                    backgroundCol = rettsLineFourNavigationCard,
-                    modifier = Modifier
-                        .width(370.dp),
-                    navTarget = NavigationEnum.NURSING_NEED,
-                    navigateToForm = navigateToForm
-                )
-                navigationCard(
-                    label = R.string.medicalOrder,
-                    backgroundCol = rettsLineFourNavigationCard,
-                    modifier = Modifier
-                        .width(370.dp),
-                    navTarget = NavigationEnum.MEDICAL_ORDER,
-                    navigateToForm = navigateToForm
-                )
-            }
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 10.dp),
-                horizontalArrangement = Arrangement.SpaceEvenly
-            ) {
-                navigationCard(
-                    label = R.string.interimJournal,
-                    backgroundCol = rettsLineFourNavigationCard,
-                    modifier = Modifier
-                        .width(760.dp),
-                    navTarget = NavigationEnum.INTERIM_JOURNAL,
-                    navigateToForm = navigateToForm
-                )
-            }
-            Row(
-                horizontalArrangement = Arrangement.End,
-                verticalAlignment = Alignment.Bottom
-            ) {
-                overViewButton(showOverview = showOverview)
-            }
-        }
-    }
-
-    /**
-     * TODO: When we have avaialable patient data, this card is supposed to display patient information
-     */
-    @Composable
-    fun InfoDisplay() {
-        Column(
+) {
+    Column(
+        modifier = Modifier,
+        verticalArrangement = Arrangement.Center
+    )
+    {
+        InfoDisplay()
+        Row(
             modifier = Modifier
                 .fillMaxWidth(),
-            verticalArrangement = Arrangement.Top
+            horizontalArrangement = Arrangement.SpaceEvenly
         ) {
-            Card(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 10.dp, start = 20.dp, end = 20.dp, top = 10.dp)
-                    .height(150.dp),
-                elevation = 10.dp
+            navigationCard(
+                label = R.string.arrivalCard,
+                backgroundCol = rettsLineOneNavigationCard,
+                modifier = Modifier.width(370.dp),
+                navTarget = NavigationEnum.ARRIVAL,
+                navigateToForm = navToArrival
             )
-            {
-                Text("Namn: Erik Bengtsson", textAlign = TextAlign.Start, fontSize = 20.sp)
-                Text("ID: 1997001122-XXXX", textAlign = TextAlign.End, fontSize = 20.sp)
-            }
+            navigationCard(
+                label = R.string.hazardCard,
+                backgroundCol = rettsLineOneNavigationCard,
+                modifier = Modifier.width(370.dp),
+                navTarget = NavigationEnum.DANGER,
+                navigateToForm = navToHazard
+            )
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 10.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        )
+        {
+            navigationCard(
+                label = R.string.contactReason,
+                backgroundCol = rettsLineTwoNavigationCard,
+                modifier = Modifier
+                    .width(240.dp),
+                navTarget = NavigationEnum.CONTACT_REASON,
+                navigateToForm = navToContactReason
+            )
+            navigationCard(
+                label = R.string.previousCare,
+                backgroundCol = rettsLineTwoNavigationCard,
+                modifier = Modifier
+                    .width(240.dp),
+                navTarget = NavigationEnum.PREVIOUS_CARE,
+                navigateToForm = navToPreviousCare
+            )
+            navigationCard(
+                label = R.string.healthHistory,
+                backgroundCol = rettsLineTwoNavigationCard,
+                modifier = Modifier
+                    .width(240.dp),
+                navTarget = NavigationEnum.HEALTH_HISTORY,
+                navigateToForm = navToHealthHistory
+            )
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 10.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        )
+        {
+            navigationCard(
+                label = R.string.healthNow,
+                backgroundCol = rettsLineTwoNavigationCard,
+                modifier = Modifier
+                    .width(370.dp),
+                navTarget = NavigationEnum.HEALTH_NOW,
+                navigateToForm = navToHealthNow
+            )
+            navigationCard(
+                label = R.string.suicideAssessment,
+                backgroundCol = rettsLineTwoNavigationCard,
+                modifier = Modifier
+                    .width(370.dp),
+                navTarget = NavigationEnum.SUICIDE_ASSESSMENT,
+                navigateToForm = navToSuicideAssessment
+            )
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 10.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            navigationCard(
+                label = R.string.nursingNeed,
+                backgroundCol = rettsLineFourNavigationCard,
+                modifier = Modifier
+                    .width(370.dp),
+                navTarget = NavigationEnum.NURSING_NEED,
+                navigateToForm = navToNursingNeed
+            )
+            navigationCard(
+                label = R.string.medicalOrder,
+                backgroundCol = rettsLineFourNavigationCard,
+                modifier = Modifier
+                    .width(370.dp),
+                navTarget = NavigationEnum.MEDICAL_ORDER,
+                navigateToForm = navToMedicalOrder
+            )
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 10.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            navigationCard(
+                label = R.string.interimJournal,
+                backgroundCol = rettsLineFourNavigationCard,
+                modifier = Modifier
+                    .width(760.dp),
+                navTarget = NavigationEnum.INTERIM_JOURNAL,
+                navigateToForm = navToInterimJournal
+            )
+        }
+        Row(
+            horizontalArrangement = Arrangement.End,
+            verticalAlignment = Alignment.Bottom
+        ) {
+            overViewButton(showOverview = showOverview)
         }
     }
+}
 
-    @Composable
-    fun navigationCard(
-        label: Int,
-        backgroundCol: Color,
-        modifier: Modifier,
-        navTarget: NavigationEnum,
-        navigateToForm: (nav: NavigationEnum) -> Unit
+/*
+  TODO: refactor these to own file
+   */
+enum class NavigationEnum {
+    ARRIVAL, DANGER, CONTACT_REASON, PREVIOUS_CARE, HEALTH_HISTORY, HEALTH_NOW, SUICIDE_ASSESSMENT, NURSING_NEED, MEDICAL_ORDER, INTERIM_JOURNAL
+}
+
+
+/**
+ * TODO: When we have avaialable patient data, this card is supposed to display patient information
+ */
+@Composable
+fun InfoDisplay() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.Top
     ) {
         Card(
-            modifier = modifier
-                .height(180.dp)
-                .clickable { navigateToForm(navTarget) },
-            backgroundColor = backgroundCol,
-        ) {
-            Text(
-                stringResource(label),
-                fontWeight = FontWeight.SemiBold,
-                textAlign = TextAlign.Center,
-                fontSize = 30.sp
-            )
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 10.dp, start = 20.dp, end = 20.dp, top = 10.dp)
+                .height(150.dp),
+            elevation = 10.dp
+        )
+        {
+            Text("Namn: Erik Bengtsson", textAlign = TextAlign.Start, fontSize = 20.sp)
+            Text("ID: 1997001122-XXXX", textAlign = TextAlign.End, fontSize = 20.sp)
         }
     }
+}
 
-    @Composable
-    fun overViewButton(
-        showOverview: (patientId: String) -> Unit = {}
+
+@Composable
+fun navigationCard(
+    label: Int,
+    backgroundCol: Color,
+    modifier: Modifier,
+    navTarget: NavigationEnum,
+    navigateToForm: () -> Unit
+) {
+    Card(
+        modifier = modifier
+            .height(180.dp)
+            .clickable { navigateToForm() },
+        backgroundColor = backgroundCol,
     ) {
-        IconButton(
-            onClick = { showOverview("") }) {
-            Icon(
-                imageVector = Icons.Rounded.MedicalInformation,
-                contentDescription = "Overview",
-                tint = colorIcon
-            )
-        }
+        Text(
+            stringResource(label),
+            fontWeight = FontWeight.SemiBold,
+            textAlign = TextAlign.Center,
+            fontSize = 30.sp
+        )
+    }
+}
+
+@Composable
+fun overViewButton(
+    showOverview: (patientId: String) -> Unit = {}
+) {
+    IconButton(
+        onClick = { showOverview("") }) {
+        Icon(
+            imageVector = Icons.Rounded.MedicalInformation,
+            contentDescription = "Overview",
+            tint = colorIcon
+        )
     }
 }
