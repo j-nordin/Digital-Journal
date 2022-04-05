163499388527c086711dedab876845c185ebd7af
axelhertzberg <axelher@student.chalmers.se>
2022-04-05 11:41:56 +0200

    LazyGrid enum checkboxes
    * Using LazyVerticalGrid for the checkboxes to get them aligned right
    * TODO: Fix the Arrival page so they using it aswell.

--- androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/ui/triage/history/HealthHistoryContent.kt
+++ androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/ui/triage/history/HealthHistoryContent.kt
@@ -1,55 +1,54 @@
 package com.EENX15_22_17.digital_journal.android.ui.triage.history
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.MutableLiveData
 import com.EENX15_22_17.digital_journal.android.R
-import com.EENX15_22_17.digital_journal.android.ui.components.EnumCheckBoxVertical
+import com.EENX15_22_17.digital_journal.android.ui.components.EnumCheckBoxLazyGrid
 
 
 //FIXME: When having multiple EnumChecbox groups the value is resetting if we click on new group.
 @Composable
-fun SomaticHealth(
-    currentValues: Set<SomaticHealthData>,
-    onChange: (values: Set<SomaticHealthData>) -> Unit,
-    modifier: Modifier = Modifier
+fun <E : Enum<*>> SomaticHealth(
+    modifier: Modifier = Modifier,
+    choices: Array<E>,
+    onChange: (values: Set<E>) -> Unit,
+    currentSelected: Set<E>,
+    labels: Map<E, String>,
 ) {
-    Column {
+
+    Column(modifier = modifier) {
         Text(
             text = stringResource(id = R.string.somaticHealth),
             fontSize = 20.sp,
             fontWeight = FontWeight.Bold,
             modifier = Modifier.padding(vertical = 16.dp)
         )
-        Row ( horizontalArrangement = Arrangement.spacedBy(60.dp)) {
-            EnumCheckBoxVertical(
-                choices = somaticHealthValues.keys.toTypedArray().sliceArray(0..4),
-                onSelectionChanged = onChange,
-                currentSelected = currentValues.toMutableSet(),
-                labels = somaticHealthValues
-            )
-            EnumCheckBoxVertical(
-                choices = somaticHealthValues.keys.toTypedArray().sliceArray(5..9),
-                onSelectionChanged = onChange,
-                currentSelected = currentValues.toMutableSet(),
-                labels = somaticHealthValues
-            )
-            EnumCheckBoxVertical(
-                choices = somaticHealthValues.keys.toTypedArray().sliceArray(10..15),
-                onSelectionChanged = onChange,
-                currentSelected = currentValues.toMutableSet(),
-                labels = somaticHealthValues
-            )
-        }
+        EnumCheckBoxLazyGrid(
+            choices = choices,
+            onSelectionChanged = onChange,
+            currentSelected = currentSelected,
+            labels =labels
+        )
     }
+}
+
+@Composable
+fun BloodInfection(
+    isBloodInfection: MutableLiveData<YesNo>,
+    onBloodInfection: (value: YesNo) -> Unit,
+) {
+    Text(
+        text = stringResource(id = R.string.bloodInfection),
+        fontSize = 20.sp,
+        fontWeight = FontWeight.Bold,
+        modifier = Modifier.padding(vertical = 16.dp)
+    )
 }
