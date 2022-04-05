944e2e4110c6a29b515e7eba76657ce5bccf5e51
axelhertzberg <axelher@student.chalmers.se>
2022-04-05 16:45:31 +0200

    HealthHistorypage done
    * This viewmodel uses LiveData

--- androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/ui/triage/history/HealthHistoryContent.kt
+++ androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/ui/triage/history/HealthHistoryContent.kt
@@ -1,5 +1,6 @@
 package com.EENX15_22_17.digital_journal.android.ui.triage.history
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
@@ -14,7 +15,7 @@ import com.EENX15_22_17.digital_journal.android.R
 import com.EENX15_22_17.digital_journal.android.ui.components.EnumCheckBoxLazyGrid
 import com.EENX15_22_17.digital_journal.android.ui.components.EnumRadioButtonsHorizontal
 import com.EENX15_22_17.digital_journal.android.dataModel.YesNo
-import com.EENX15_22_17.digital_journal.android.dataModel.yesNoLabels
+import com.EENX15_22_17.digital_journal.android.ui.components.TitledTextField
 
 @Composable
 fun <E : Enum<*>> SomaticHealth(
@@ -47,6 +48,8 @@ fun BloodInfection(
     modifier: Modifier = Modifier,
     isBloodInfection: YesNo,
     onBloodInfection: (value: YesNo) -> Unit,
+    choices: Array<YesNo>,
+    labels: Map<YesNo, String>
 ) {
     Column(
         modifier = modifier
@@ -58,11 +61,181 @@ fun BloodInfection(
             modifier = Modifier.padding(vertical = 16.dp)
         )
         EnumRadioButtonsHorizontal(
-            choices = yesNoLabels.keys.toTypedArray(),
-            labels = yesNoLabels,
+            choices = choices,
+            labels = labels,
             currentChoice = isBloodInfection,
             onSelection = onBloodInfection
         )
     }
-
 }
+
+@Composable
+fun BloodInfectionType(
+    modifier: Modifier = Modifier,
+    onChangeText: (textValue: String) -> Unit,
+    textValue: String
+) {
+    Column(
+        modifier = modifier
+    ) {
+        Text(
+            text = stringResource(id = R.string.bloodInfectionType),
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(vertical = 16.dp)
+        )
+        TitledTextField(
+            title = "Typ",
+            onChangeText = onChangeText,
+            textValue = textValue
+        )
+    }
+}
+
+@Composable
+fun Hypersensitivity(
+    modifier: Modifier = Modifier,
+    isHypersensitiv: YesNo,
+    onHypersensitiv: (value: YesNo) -> Unit,
+    choices: Array<YesNo>,
+    labels: Map<YesNo, String>
+) {
+    Column(
+        modifier = modifier
+    ) {
+        Text(
+            text = stringResource(id = R.string.hypersensitivity),
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(vertical = 16.dp)
+        )
+        EnumRadioButtonsHorizontal(
+            choices = choices,
+            labels = labels,
+            currentChoice = isHypersensitiv,
+            onSelection = onHypersensitiv
+        )
+    }
+}
+
+@Composable
+fun HypersensitivityType(
+    modifier: Modifier = Modifier,
+    onChangeText: (textValue: String) -> Unit,
+    textValue: String
+) {
+    Column(
+        modifier = modifier
+    ) {
+        Text(
+            text = stringResource(id = R.string.hypersensitivityType),
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(vertical = 16.dp)
+        )
+        TitledTextField(
+            title = "Typ",
+            onChangeText = onChangeText,
+            textValue = textValue
+        )
+    }
+}
+
+@Composable
+fun Multiresistant(
+    modifier: Modifier = Modifier,
+    isMultiresistant: YesNo,
+    onMultiresistant: (value: YesNo) -> Unit,
+    choices: Array<YesNo>,
+    labels: Map<YesNo, String>
+) {
+    Column(
+        modifier = modifier
+    ) {
+        Text(
+            text = stringResource(id = R.string.multiresistant),
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(vertical = 16.dp)
+        )
+        EnumRadioButtonsHorizontal(
+            choices = choices,
+            labels = labels,
+            currentChoice = isMultiresistant,
+            onSelection = onMultiresistant
+        )
+    }
+}
+
+@Composable
+fun SuspicionGE(
+    modifier: Modifier = Modifier,
+    isSuspicionGE: YesNo,
+    onSuspicionGE: (value: YesNo) -> Unit,
+    choices: Array<YesNo>,
+    labels: Map<YesNo, String>
+) {
+    Column(
+        modifier = modifier
+    ) {
+        Text(
+            text = stringResource(id = R.string.suspicionGE),
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(vertical = 16.dp)
+        )
+        EnumRadioButtonsHorizontal(
+            choices = choices,
+            labels = labels,
+            currentChoice = isSuspicionGE,
+            onSelection = onSuspicionGE
+        )
+    }
+}
+
+@Composable
+fun NursesNeeds(
+    modifier: Modifier = Modifier,
+    isNursesNeeds: YesNo,
+    onNursesNeeds: (value: YesNo) -> Unit,
+    choices: Array<YesNo>,
+    labels: Map<YesNo, String>
+) {
+    Column(
+        modifier = modifier
+    ) {
+        Text(
+            text = stringResource(id = R.string.nursesNeeds),
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(vertical = 16.dp)
+        )
+        EnumRadioButtonsHorizontal(
+            choices = choices,
+            labels = labels,
+            currentChoice = isNursesNeeds,
+            onSelection = onNursesNeeds
+        )
+    }
+}
+
+@Composable
+fun <E : Enum<E>> NursesNeedsAlternative(
+    modifier: Modifier = Modifier,
+    currentValues: Set<E>,
+    setValues: (value: Set<E>) -> Unit,
+    choices: Array<E>,
+    labels: Map<E, String>,
+) {
+    Box(modifier = modifier.padding(start = 40.dp)) {
+        EnumCheckBoxLazyGrid(
+            choices = choices,
+            onSelectionChanged = setValues,
+            currentSelected = currentValues,
+            labels = labels,
+            gridLayout = GridCells.Adaptive(250.dp)
+        )
+    }
+}
+
+
--- androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/ui/triage/history/HealthHistoryViewModel.kt
+++ androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/ui/triage/history/HealthHistoryViewModel.kt
@@ -2,6 +2,7 @@ package com.EENX15_22_17.digital_journal.android.ui.triage.history
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.EENX15_22_17.digital_journal.android.dataModel.NursesNeeds
 import com.EENX15_22_17.digital_journal.android.dataModel.YesNo
 
 class HealthHistoryViewModel : ViewModel() {
@@ -19,23 +20,47 @@ class HealthHistoryViewModel : ViewModel() {
         MutableLiveData<String>("")
     }
 
+    val isHyperSensitive: MutableLiveData<YesNo> by lazy {
+        MutableLiveData<YesNo>(YesNo.UNKOWN)
+    }
 
-   /* val earlierHealthy: MutableLiveData<YesNo> by lazy { MutableLiveData<YesNo>() }
-    val delirium: MutableLiveData<YesNo> by lazy { MutableLiveData<YesNo>() }
-    val cardiovascular: MutableLiveData<YesNo> by lazy { MutableLiveData<YesNo>() }
-    val ep: MutableLiveData<YesNo> by lazy { MutableLiveData<YesNo>() }
-    val cerebrovascular: MutableLiveData<YesNo> by lazy { MutableLiveData<YesNo>() }
-    val liverFailure: MutableLiveData<YesNo> by lazy { MutableLiveData<YesNo>() }
-    val tablet: MutableLiveData<YesNo> by lazy { MutableLiveData<YesNo>() }
-    val operation: MutableLiveData<YesNo> by lazy { MutableLiveData<YesNo>() }
-    val heartFailure: MutableLiveData<YesNo> by lazy { MutableLiveData<YesNo>() }
-    val kidneyFailure: MutableLiveData<YesNo> by lazy { MutableLiveData<YesNo>() }
-    val lungDisease: MutableLiveData<YesNo> by lazy { MutableLiveData<YesNo>() }
-    val diabetes: MutableLiveData<YesNo> by lazy { MutableLiveData<YesNo>() }
-    val insulin: MutableLiveData<YesNo> by lazy { MutableLiveData<YesNo>() }
-    val bleedingDisorder: MutableLiveData<YesNo> by lazy { MutableLiveData<YesNo>() }
-    val malignancy: MutableLiveData<YesNo> by lazy { MutableLiveData<YesNo>() }
-    val other: MutableLiveData<YesNo> by lazy { MutableLiveData<YesNo>()*/
+    val hyperSensitiveType: MutableLiveData<String> by lazy {
+        MutableLiveData<String>("")
+    }
+
+    val isMultiresistant: MutableLiveData<YesNo> by lazy {
+        MutableLiveData<YesNo>(YesNo.UNKOWN)
+    }
+
+    val suspicionGE: MutableLiveData<YesNo> by lazy {
+        MutableLiveData<YesNo>(YesNo.UNKOWN)
+    }
+
+    val isNursesNeed: MutableLiveData<YesNo> by lazy {
+        MutableLiveData<YesNo>(YesNo.UNKOWN)
+    }
+
+    val nursesNeedsAlternatives: MutableLiveData<Set<NursesNeeds>> by lazy {
+        MutableLiveData<Set<NursesNeeds>>(setOf<NursesNeeds>())
+    }
+
+
+    /* val earlierHealthy: MutableLiveData<YesNo> by lazy { MutableLiveData<YesNo>() }
+     val delirium: MutableLiveData<YesNo> by lazy { MutableLiveData<YesNo>() }
+     val cardiovascular: MutableLiveData<YesNo> by lazy { MutableLiveData<YesNo>() }
+     val ep: MutableLiveData<YesNo> by lazy { MutableLiveData<YesNo>() }
+     val cerebrovascular: MutableLiveData<YesNo> by lazy { MutableLiveData<YesNo>() }
+     val liverFailure: MutableLiveData<YesNo> by lazy { MutableLiveData<YesNo>() }
+     val tablet: MutableLiveData<YesNo> by lazy { MutableLiveData<YesNo>() }
+     val operation: MutableLiveData<YesNo> by lazy { MutableLiveData<YesNo>() }
+     val heartFailure: MutableLiveData<YesNo> by lazy { MutableLiveData<YesNo>() }
+     val kidneyFailure: MutableLiveData<YesNo> by lazy { MutableLiveData<YesNo>() }
+     val lungDisease: MutableLiveData<YesNo> by lazy { MutableLiveData<YesNo>() }
+     val diabetes: MutableLiveData<YesNo> by lazy { MutableLiveData<YesNo>() }
+     val insulin: MutableLiveData<YesNo> by lazy { MutableLiveData<YesNo>() }
+     val bleedingDisorder: MutableLiveData<YesNo> by lazy { MutableLiveData<YesNo>() }
+     val malignancy: MutableLiveData<YesNo> by lazy { MutableLiveData<YesNo>() }
+     val other: MutableLiveData<YesNo> by lazy { MutableLiveData<YesNo>()*/
 }
 
 
