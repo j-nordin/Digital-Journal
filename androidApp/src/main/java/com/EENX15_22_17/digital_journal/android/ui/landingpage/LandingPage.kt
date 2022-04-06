e6b8d714e9b83d70b3f2170ace4bd178d4124e91
Rickard Lantz <rickard.lantz@predicare.se>
2022-04-06 21:05:48 +0200

    Refactor Landing page to use Figma-styles.
    This does not fix card spacing

--- androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/ui/landingpage/LandingPage.kt
+++ androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/ui/landingpage/LandingPage.kt
@@ -2,25 +2,25 @@ package com.EENX15_22_17.digital_journal.android.ui.landingpage
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.MedicalInformation
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.lerp
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import com.EENX15_22_17.digital_journal.android.R
-import com.EENX15_22_17.digital_journal.android.ui.theme.rettsLineFourNavigationCard
-import com.EENX15_22_17.digital_journal.android.ui.theme.rettsLineOneNavigationCard
-import com.EENX15_22_17.digital_journal.android.ui.theme.rettsLineTwoNavigationCard
-import androidx.compose.material.icons.Icons
-import androidx.compose.ui.Alignment
-import androidx.compose.material.*
-import androidx.compose.material.icons.rounded.MedicalInformation
 import androidx.compose.ui.unit.sp
+import com.EENX15_22_17.digital_journal.android.R
+import com.EENX15_22_17.digital_journal.android.ui.theme.Colors
+import com.EENX15_22_17.digital_journal.android.ui.theme.TextStyles
 import com.EENX15_22_17.digital_journal.android.ui.theme.colorIcon
 
 
@@ -51,14 +51,14 @@ fun LandingPage(
             horizontalArrangement = Arrangement.SpaceEvenly
         ) {
             NavigationCard(
-                label = R.string.arrivalCard,
-                backgroundCol = rettsLineOneNavigationCard,
+                label = stringResource(R.string.arrivalCard),
+                titleColor = Colors.arrivalPrimary,
                 modifier = Modifier.width(370.dp),
                 navigateToForm = navToArrival
             )
             NavigationCard(
-                label = R.string.hazardCard,
-                backgroundCol = rettsLineOneNavigationCard,
+                label = stringResource(R.string.hazardCard),
+                titleColor = Colors.arrivalPrimary,
                 modifier = Modifier.width(370.dp),
                 navigateToForm = navToHazard
             )
@@ -71,22 +71,22 @@ fun LandingPage(
         )
         {
             NavigationCard(
-                label = R.string.contactReason,
-                backgroundCol = rettsLineTwoNavigationCard,
+                label = stringResource(R.string.contactReason),
+                titleColor = Colors.triagePrimary,
                 modifier = Modifier
                     .width(240.dp),
                 navigateToForm = navToContactReason
             )
             NavigationCard(
-                label = R.string.previousCare,
-                backgroundCol = rettsLineTwoNavigationCard,
+                label = stringResource(R.string.previousCare),
+                titleColor = Colors.triagePrimary,
                 modifier = Modifier
                     .width(240.dp),
                 navigateToForm = navToPreviousCare
             )
             NavigationCard(
-                label = R.string.healthHistory,
-                backgroundCol = rettsLineTwoNavigationCard,
+                label = stringResource(R.string.healthHistory),
+                titleColor = Colors.triagePrimary,
                 modifier = Modifier
                     .width(240.dp),
                 navigateToForm = navToHealthHistory
@@ -100,15 +100,15 @@ fun LandingPage(
         )
         {
             NavigationCard(
-                label = R.string.healthNow,
-                backgroundCol = rettsLineTwoNavigationCard,
+                label = stringResource(R.string.healthNow),
+                titleColor = Colors.triagePrimary,
                 modifier = Modifier
                     .width(370.dp),
                 navigateToForm = navToHealthNow
             )
             NavigationCard(
-                label = R.string.suicideAssessment,
-                backgroundCol = rettsLineTwoNavigationCard,
+                label = stringResource(R.string.suicideAssessment),
+                titleColor = Colors.triagePrimary,
                 modifier = Modifier
                     .width(370.dp),
                 navigateToForm = navToSuicideAssessment
@@ -121,15 +121,15 @@ fun LandingPage(
             horizontalArrangement = Arrangement.SpaceEvenly
         ) {
             NavigationCard(
-                label = R.string.nursingNeed,
-                backgroundCol = rettsLineFourNavigationCard,
+                label = stringResource(R.string.nursingNeed),
+                titleColor = Colors.treatmentPrimary,
                 modifier = Modifier
                     .width(370.dp),
                 navigateToForm = navToNursingNeed
             )
             NavigationCard(
-                label = R.string.medicalOrder,
-                backgroundCol = rettsLineFourNavigationCard,
+                label = stringResource(R.string.medicalOrder),
+                titleColor = Colors.treatmentPrimary,
                 modifier = Modifier
                     .width(370.dp),
                 navigateToForm = navToMedicalOrder
@@ -142,8 +142,8 @@ fun LandingPage(
             horizontalArrangement = Arrangement.SpaceEvenly
         ) {
             NavigationCard(
-                label = R.string.interimJournal,
-                backgroundCol = rettsLineFourNavigationCard,
+                label = stringResource(R.string.interimJournal),
+                titleColor = Colors.treatmentPrimary,
                 modifier = Modifier
                     .width(760.dp),
                 navigateToForm = navToInterimJournal
@@ -186,23 +186,27 @@ fun InfoDisplay() {
 
 @Composable
 fun NavigationCard(
-    label: Int,
-    backgroundCol: Color,
-    modifier: Modifier,
-    navigateToForm: () -> Unit
+    modifier: Modifier = Modifier,
+    label: String = "Title",
+    titleColor: Color = Color.Blue,
+    backgroundColor: Color = lerp(titleColor, Color.White, 0.70f),
+    navigateToForm: () -> Unit = {}
 ) {
     Card(
         modifier = modifier
             .height(180.dp)
             .clickable { navigateToForm() },
-        backgroundColor = backgroundCol,
+        backgroundColor = backgroundColor,
+        elevation = 4.dp,
+        shape = RoundedCornerShape(10.dp)
     ) {
-        Text(
-            stringResource(label),
-            fontWeight = FontWeight.SemiBold,
-            textAlign = TextAlign.Center,
-            fontSize = 30.sp
-        )
+        Row(horizontalArrangement = Arrangement.Center, verticalAlignment = Alignment.CenterVertically) {
+            Text(
+                text = label,
+                style = TextStyles.cardTitle,
+                color = titleColor
+            )
+        }
     }
 }
 
