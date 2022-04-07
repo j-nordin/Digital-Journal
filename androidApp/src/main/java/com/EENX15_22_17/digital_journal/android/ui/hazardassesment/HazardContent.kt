5560ede106106d0901bcce66769335be78b56b58
erikbengtsson <eribeng@student.chalmers.se>
2022-04-07 13:14:09 +0200

    added nr of BVC to the hazardModel

--- androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/ui/hazardassesment/HazardContent.kt
+++ androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/ui/hazardassesment/HazardContent.kt
@@ -1,11 +1,21 @@
 package com.EENX15_22_17.digital_journal.android.ui.hazardassesment
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType.Companion.Text
 import androidx.compose.ui.unit.dp
 import com.EENX15_22_17.digital_journal.android.R
 import com.EENX15_22_17.digital_journal.android.ui.components.*
@@ -49,9 +59,10 @@ fun HazardousBehaviours(
     choices: Array<DangerBehaviors>,
     onChange: (values: Set<DangerBehaviors>) -> Unit,
     currentSelected: Set<DangerBehaviors>,
-    labels: Map<DangerBehaviors, String>
-
+    labels: Map<DangerBehaviors, String>,
+    bvcCounter: Int
 ) {
+    var updatedBvcCount by rememberSaveable { mutableStateOf(bvcCounter) }
     EnumCheckBoxLazyGrid(
         choices = choices,
         onSelectionChanged = onChange,
@@ -59,5 +70,17 @@ fun HazardousBehaviours(
         labels = labels,
         gridLayout = GridCells.Adaptive(300.dp)
     )
+}
+
+@Composable
+fun BVCfield(
+    BVC: Int
+) {
+    Box(
+        modifier = Modifier
+    )
+    {
+        Text("Number of BVC: $BVC")
+    }
 }
 
