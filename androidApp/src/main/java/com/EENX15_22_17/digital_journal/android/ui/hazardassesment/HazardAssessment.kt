2d7a72831ce0ae211ef9c4c82ece63d9794710e7
erikbengtsson <eribeng@student.chalmers.se>
2022-04-08 14:34:29 +0200

    added titled section to hazardAssessment page and made a card for bbv summary, please note the fixmes on hazardAssessment page

--- androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/ui/hazardassesment/HazardAssessment.kt
+++ androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/ui/hazardassesment/HazardAssessment.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -26,9 +27,13 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.EENX15_22_17.digital_journal.android.ui.hazardassesment.*
 import com.EENX15_22_17.digital_journal.android.R
+import com.EENX15_22_17.digital_journal.android.screens.treatment.ordination.OrdinationScreen
+import com.EENX15_22_17.digital_journal.android.ui.DetailPageWrapper
 import com.EENX15_22_17.digital_journal.android.ui.arrivalpage.ArrivalViewModel
 import com.EENX15_22_17.digital_journal.android.ui.components.EnumCheckBoxLazyGrid
+import com.EENX15_22_17.digital_journal.android.ui.components.TitledSection
 import com.EENX15_22_17.digital_journal.android.ui.components.TitledTextField
+import com.EENX15_22_17.digital_journal.android.ui.theme.Colors
 import com.EENX15_22_17.digital_journal.android.ui.theme.borderColor
 import com.EENX15_22_17.digital_journal.android.ui.components.EnumCheckBoxLazyGrid
 import com.EENX15_22_17.digital_journal.android.ui.components.TitledTextField
@@ -41,69 +46,78 @@ fun HazardAssessment(
     hazardViewModel: HazardViewModel = HazardViewModel(),
     visitId: String
 ) {
-    var bvcNr by rememberSaveable { mutableStateOf(0) }
-    val rowModifier: Modifier = Modifier
-        .fillMaxWidth()
-        .padding(vertical = 6.dp, horizontal = 2.dp)
-        .border(
-            border = BorderStroke(
-                2.dp,
-                borderColor
-            ),
-            shape = RoundedCornerShape(8.dp)
-        )
-        .padding(horizontal = 16.dp, vertical = 12.dp)
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally
+    DetailPageWrapper(
+        title = stringResource(id = R.string.dangerAssessmentHeader),
+        titleColor = Colors.treatmentPrimary,
+        onBackClicked = navBack,
+        onMenuClicked = {}
     ) {
-        Text(
-            text = stringResource(id = R.string.dangerAssessmentHeader),
-            fontWeight = FontWeight.SemiBold,
-            textAlign = TextAlign.Center,
-            fontSize = 30.sp
-        )
-        Row(
-            modifier = rowModifier
-        ) {
-            EnumCheckBoxLazyGrid(
-                choices = dangerTypes.keys.toTypedArray(),
-                onSelectionChanged = { hazardViewModel.hazardStates.initialAssessment = it },
-                currentSelected = hazardViewModel.hazardStates.initialAssessment,
-                labels = dangerTypes,
-                gridLayout = GridCells.Adaptive(300.dp)
+        //FIXME, when state issue is solved, this must be updated when clicking on specified behavioir checkboxes
+        var bvcNr by rememberSaveable { mutableStateOf(0) }
+        val rowModifier: Modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 6.dp, horizontal = 2.dp)
+            .border(
+                border = BorderStroke(
+                    2.dp,
+                    borderColor
+                ),
+                shape = RoundedCornerShape(8.dp)
             )
-        }
-        Text(stringResource(id = R.string.BVC))
-        Row(
-            modifier = rowModifier
+            .padding(horizontal = 16.dp, vertical = 12.dp)
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            EnumCheckBoxLazyGrid(
-                choices = dangerBehaviors.keys.toTypedArray(),
-                onSelectionChanged = { hazardViewModel.hazardStates.specifiedBehavior = it },
-                currentSelected = hazardViewModel.hazardStates.specifiedBehavior,
-                labels = dangerBehaviors,
-                gridLayout = GridCells.Adaptive(300.dp)
-            )
-        }
-        Row(
-            modifier = rowModifier
-        ) {
-            Text(text = "Number of BVC: $bvcNr", textAlign = TextAlign.Center)
-        }
-        if (bvcNr > 1) {
-            Row() {
-                TitledTextField(
-                    modifier = Modifier
-                        .width(600.dp)
-                        .height(250.dp),
-                    title = stringResource(id = R.string.hazardActionTaken),
-                    onChangeText = { hazardViewModel.hazardStates.takenActions = it },
-                    textValue = hazardViewModel.hazardStates.takenActions
+            TitledSection(title = "Initial Bedömning") {
+                EnumCheckBoxLazyGrid(
+                    choices = dangerTypes.keys.toTypedArray(),
+                    onSelectionChanged = { hazardViewModel.hazardStates.initialAssessment = it },
+                    currentSelected = hazardViewModel.hazardStates.initialAssessment,
+                    labels = dangerTypes,
+                    gridLayout = GridCells.Adaptive(270.dp)
                 )
             }
-        }
-        Button(onClick = navBack) {
-            Text(text = "Back")
+            TitledSection(title = stringResource(id = R.string.BVC))
+            {
+                EnumCheckBoxLazyGrid(
+                    choices = dangerBehaviors.keys.toTypedArray(),
+                    onSelectionChanged = { hazardViewModel.hazardStates.specifiedBehavior = it },
+                    currentSelected = hazardViewModel.hazardStates.specifiedBehavior,
+                    labels = dangerBehaviors,
+                    gridLayout = GridCells.Adaptive(270.dp)
+                )
+            }
+            TitledSection(title = "Summary") {
+                Card(
+                    elevation = 2.dp,
+                    backgroundColor = Colors.treatmentPrimary,
+
+                    ) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Text(
+                            text = "Number of BVC: $bvcNr",
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.padding(8.dp)
+                        )
+                    }
+                }
+            }
+            // FIXME When state issue is solved, this will only appear when BBV >= 2.
+            if (bvcNr == 0) {
+                Row() {
+                    TitledTextField(
+                        modifier = Modifier
+                            .width(600.dp)
+                            .height(250.dp),
+                        title = stringResource(id = R.string.hazardActionTaken),
+                        onChangeText = { hazardViewModel.hazardStates.takenActions = it },
+                        textValue = hazardViewModel.hazardStates.takenActions
+                    )
+                }
+            }
         }
         val rowModifier: Modifier = Modifier
             .fillMaxWidth()
