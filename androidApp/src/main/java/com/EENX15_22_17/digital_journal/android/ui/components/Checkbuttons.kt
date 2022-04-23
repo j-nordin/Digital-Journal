efe2c7ace58f15628b6c0830c43566e1531dabba
Rickard Lantz <rickard.lantz@predicare.se>
2022-04-23 12:07:09 +0200

    Refactor checkboxes for better code reuse and to follow Figma.
    * Checkbox-boxes are still not the correct size, we'll probably hav eto create a custom component for that.
    * Padding has been removed from the checkboxes themselves, as this should be handled by the parent container.
        - This will cause some visual inconsistency before this is reimplemented on the call sites.

--- androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/ui/components/Checkbuttons.kt
+++ androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/ui/components/Checkbuttons.kt
@@ -1,208 +1,135 @@
 package com.EENX15_22_17.digital_journal.android.ui.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyGridScope
 import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Checkbox
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.EENX15_22_17.digital_journal.android.ui.theme.checkBoxColor
+import com.EENX15_22_17.digital_journal.android.ui.theme.TextStyles
+import com.EENX15_22_17.digital_journal.android.ui.theme.checkboxBackgroundShape
+import com.EENX15_22_17.digital_journal.android.ui.theme.checkboxBoxSize
 import com.EENX15_22_17.digital_journal.android.ui.theme.colorCheckBoxBeta
+import modifyIf
+
 
 @Composable
-fun <E : Enum<*>> EnumCheckBox(
+fun <E : Enum<*>> EnumCheckboxesHorizontal(
     choices: Array<E>,
-    onSelectionChanged: (selected: Set<E>) -> Unit,
-    selection: Set<E>,
-    labels: Map<E, String>
-) {
+    labels: Map<E, String>,
+    selection: MutableSet<E>,
+    onSelectionChanged: (selected: Set<E>) -> Unit
+) = EnumCheckboxesLazyGrid(
+    choices = choices,
+    labels = labels,
+    selection = selection,
+    onSelectionChanged = onSelectionChanged,
+    gridLayout = GridCells.Fixed(1),
+    isHorizontal = true
+)
 
+@Composable
+fun <E : Enum<*>> EnumCheckboxesVertical(
+    choices: Array<E>,
+    labels: Map<E, String>,
+    selection: MutableSet<E>,
+    onSelectionChanged: (selected: Set<E>) -> Unit
+) = EnumCheckboxesLazyGrid(
+    choices = choices,
+    labels = labels,
+    selection = selection,
+    onSelectionChanged = onSelectionChanged,
+    gridLayout = GridCells.Fixed(1),
+    isHorizontal = false
+)
+
+
+@Composable
+fun <E : Enum<*>> EnumCheckboxesLazyGrid(
+    choices: Array<E>,
+    labels: Map<E, String>,
+    selection: Set<E>,
+    onSelectionChanged: (selected: Set<E>) -> Unit,
+    gridLayout: GridCells,
+    isHorizontal: Boolean = false,
+    checkboxSpacing: Dp = 8.dp
+) {
     val currentSelection: MutableSet<E> = selection.toMutableSet()
-    Column {
-        choices.forEach { choice ->
-            var isChecked by rememberSaveable { mutableStateOf(choice in selection) }
-            Text(text = labels[choice] ?: choice.name, textAlign = TextAlign.Center)
-            Box(
-                modifier = Modifier.background(colorCheckBoxBeta, CircleShape)
-            ) {
-                Checkbox(
-                    checked = isChecked,
-                    onCheckedChange = {
-                        isChecked = when (it) {
-                            false -> {
-                                currentSelection.remove(choice)
-                                false
-                            }
-                            true -> {
-                                currentSelection.add(choice)
-                                true
-                            }
-                        }
-                        onSelectionChanged(selection.toSet())
+    val content: LazyGridScope.() -> Unit = {
+        items(choices.size) { index ->
+            var isChecked by rememberSaveable { mutableStateOf(choices[index] in currentSelection) }
+            LabeledCheckbox(
+                label = labels[choices[index]] ?: choices[index].name,
+                checked = isChecked,
+                onCheckedChange = { checked ->
+                    isChecked = checked
+                    when (checked) {
+                        false -> currentSelection.remove(choices[index])
+                        true -> currentSelection.add(choices[index])
                     }
-                )
-            }
+                    onSelectionChanged(currentSelection.toSet())
+                }
+            )
         }
     }
-}
-
-@Composable
-fun <E : Enum<*>> EnumCheckBoxHorizontal(
-    choices: Array<E>,
-    onSelectionChanged: (selected: Set<E>) -> Unit,
-    currentSelected: MutableSet<E>,
-    labels: Map<E, String>
-) {
-    Row(modifier = Modifier.padding(2.dp), horizontalArrangement = Arrangement.SpaceEvenly) {
-        EnumCheckBox(
-            choices = choices,
-            onSelectionChanged = onSelectionChanged,
-            selection = currentSelected,
-            labels = labels
-        )
+    val spacing = Arrangement.spacedBy(checkboxSpacing)
+    when {
+        isHorizontal -> LazyHorizontalGrid(rows = gridLayout, content = content, verticalArrangement = spacing, horizontalArrangement = spacing)
+        else -> LazyVerticalGrid(columns = gridLayout, content = content, verticalArrangement = spacing, horizontalArrangement = spacing)
     }
 }
-
-@Composable
-fun <E : Enum<*>> EnumCheckBoxVertical(
-    choices: Array<E>,
-    onSelectionChanged: (selected: Set<E>) -> Unit,
-    currentSelected: MutableSet<E>,
-    labels: Map<E, String>
-) {
-    Column(modifier = Modifier.padding(2.dp)) {
-        EnumCheckBox(
-            choices = choices,
-            onSelectionChanged = onSelectionChanged,
-            selection = currentSelected,
-            labels = labels
-        )
-    }
-}
-
-
-@Composable
-fun <E : Enum<*>> EnumCheckBoxLazyGrid(
-    choices: Array<E>,
-    onSelectionChanged: (selected: Set<E>) -> Unit,
-    currentSelected: Set<E>,
-    labels: Map<E, String>,
-    gridLayout: GridCells,
-    circleBgColor: Color = checkBoxColor,
-    textFontSize: Int = 20,
-    isHorizontal: Boolean = false
-) {
-    val currentSelection: MutableSet<E> = currentSelected.toMutableSet()
-    if (isHorizontal) {
-        LazyHorizontalGrid(
-            rows = gridLayout,
-            content = {
-                items(choices.size) { index ->
-                    var isChecked by rememberSaveable { mutableStateOf(choices[index] in currentSelection) }
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                        modifier = Modifier.padding(4.dp)
-                    ) {
-                        Box(
-                            modifier = Modifier.background(colorCheckBoxBeta, CircleShape)
-                        ) {
-                            Checkbox(
-                                checked = isChecked,
-                                onCheckedChange = { checked ->
-                                    isChecked = checked
-                                    when (checked) {
-                                        false -> currentSelection.remove(choices[index])
-                                        true -> currentSelection.add(choices[index])
-                                    }
-                                    onSelectionChanged(currentSelection.toSet())
-                                }
-                            )
-                        }
-                        Text(
-                            text = labels[choices[index]] ?: choices[index].name,
-                            textAlign = TextAlign.Start,
-                            modifier = Modifier.padding(start = 8.dp),
-                            fontSize = textFontSize.sp
-                        )
-                    }
-                }
-
-            }
-        )
-    } else {
-        LazyVerticalGrid(
-            columns = gridLayout,
-            content = {
-                items(choices.size) { index ->
-                    var isChecked by rememberSaveable { mutableStateOf(choices[index] in currentSelection) }
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                        modifier = Modifier.padding(4.dp)
-                    ) {
-                        Box(
-                            modifier = Modifier.background(colorCheckBoxBeta, CircleShape)
-                        ) {
-                            Checkbox(
-                                checked = isChecked,
-                                onCheckedChange = { checked ->
-                                    isChecked = checked
-                                    when (checked) {
-                                        false -> currentSelection.remove(choices[index])
-                                        true -> currentSelection.add(choices[index])
-                                    }
-                                    onSelectionChanged(currentSelection.toSet())
-                                }
-                            )
-                        }
-                        Text(
-                            text = labels[choices[index]] ?: choices[index].name,
-                            textAlign = TextAlign.Start,
-                            modifier = Modifier.padding(start = 8.dp),
-                            fontSize = textFontSize.sp
-                        )
-                    }
-                }
-            }
-        )
-    }
-}
-
 
 @Composable
 fun LabeledCheckbox(
     label: String,
     checked: Boolean,
+    clickableText: Boolean = false,
     onCheckedChange: (isChecked: Boolean) -> Unit
 ) {
     var isChecked by rememberSaveable { mutableStateOf(checked) }
-    Row {
-        Box(
-            modifier = Modifier.background(colorCheckBoxBeta, CircleShape)
-        ) {
+
+    fun changeState(newState: Boolean? = null) {
+        isChecked = newState ?: !isChecked
+        onCheckedChange(isChecked)
+    }
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Start
+    ) {
+        Box(Modifier.background(colorCheckBoxBeta, checkboxBackgroundShape)) {
             Checkbox(
                 checked = isChecked,
-                onCheckedChange = {
-                    isChecked = it
-                    onCheckedChange(isChecked)
-                }
+                onCheckedChange = ::changeState
             )
         }
         Text(
-            modifier = Modifier.padding(start = 4.dp, top = 14.dp),
+            modifier = Modifier
+                .padding(start = 8.dp)
+                .modifyIf(clickableText, Modifier.clickable { changeState() }),
             text = label,
-            textAlign = TextAlign.Center
+            style = TextStyles.checkboxLabel,
+            textAlign = TextAlign.Start
         )
     }
 }
 
+@Composable
+@Preview
+private fun LabeledCheckboxPreview() = LabeledCheckbox(
+    label = "Long checkbox label that could potentially span multiple lines in case there is not a wide enough container for it",
+    checked = true,
+    onCheckedChange = {}
+)
