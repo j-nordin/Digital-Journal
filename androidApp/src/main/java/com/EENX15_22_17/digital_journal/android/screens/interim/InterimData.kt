7ebbf079d300c4e6fe5263e35e74b20471fae41b
axelhertzberg <axelher@student.chalmers.se>
2022-04-26 10:44:50 +0200

    First field of interimJournalPage done

--- androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/screens/interim/InterimData.kt
+++ androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/screens/interim/InterimData.kt
@@ -1,6 +1,7 @@
 package com.EENX15_22_17.digital_journal.android.screens.interim
 
 import com.EENX15_22_17.digital_journal.android.dataModel.YesAndNoAndNoAnswer
+import se.predicare.journal.screens.InterimJournalDto
 
 data class InterimData(
     var patientDecision: Set<PatientDecision>,
@@ -24,6 +25,23 @@ enum class PatientDecision {
     PATIENT_REMITTED,
     PATIENT_HOSPITALIZED
 }
+
+val patientDecision = mapOf<InterimJournalDto.PatientAction, String>(
+    InterimJournalDto.PatientAction.PATIENT_SENT_HOME to "Patienten går hem",
+    InterimJournalDto.PatientAction.PATIENT_REMITTED to "Patienten remitterad",
+    InterimJournalDto.PatientAction.PATIENT_HOSPITALIZED to "Inläggning avdelning"
+)
+
+val patientDecisionTextfields = mapOf<InterimJournalDto.PatientAction, Boolean>(
+    InterimJournalDto.PatientAction.PATIENT_SENT_HOME to false,
+    InterimJournalDto.PatientAction.PATIENT_REMITTED to true,
+    InterimJournalDto.PatientAction.PATIENT_HOSPITALIZED to true
+)
+
+val patientDecisionTitles = mapOf<InterimJournalDto.PatientAction, String>(
+    InterimJournalDto.PatientAction.PATIENT_REMITTED to "Till vård",
+    InterimJournalDto.PatientAction.PATIENT_HOSPITALIZED to "Till avdelning"
+)
 
 enum class InterimLaw {
     HSL,
