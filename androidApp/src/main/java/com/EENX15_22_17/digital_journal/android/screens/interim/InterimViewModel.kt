2958c897bcc58338f207e3be67d41360c53ca26b
axelhertzberg <axelher@student.chalmers.se>
2022-04-26 15:34:23 +0200

    Interim journal page done
    * Added some TODO:s that not simular to figma. This because i don't know how to implment it
    * Temporary data in viewModel until InterimDto has the data
    * The viewmodel now recrates everytime we enter tha page. Didn't add a TODO on that because we probably use DI.

--- androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/screens/interim/InterimViewModel.kt
+++ androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/screens/interim/InterimViewModel.kt
@@ -4,9 +4,16 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import se.predicare.journal.data.YesNoQuestion
 import se.predicare.journal.screens.InterimJournalDto
 
-class InterimViewModel: ViewModel() {
+class InterimViewModel : ViewModel() {
     var interimStates by mutableStateOf(InterimJournalDto())
+
+    //TODO: Remove when they are included in InterimDto
+    var department by mutableStateOf("")
+    var ssk by mutableStateOf("")
+    var time by mutableStateOf("")
+    var admission by mutableStateOf(YesNoQuestion.UNANSWERED)
 
 }
