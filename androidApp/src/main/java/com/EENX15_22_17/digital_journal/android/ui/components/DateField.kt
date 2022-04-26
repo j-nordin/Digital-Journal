64ae9d8428408ed0a9139dd3b7f468039cb177d2
Jonas Nordin <jonnordi@student.chalmers.se>
2022-04-26 16:27:57 +0200

    Change position on clickable icon to change date

---
+++ androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/ui/components/DateField.kt
@@ -0,0 +1,63 @@
+package com.EENX15_22_17.digital_journal.android.ui.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.EditCalendar
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun TitledDateField(
+    title: String,
+    onChangeDate: (date: String) -> Unit,
+    value: String,
+    modifier: Modifier = Modifier,
+    isEnabled: Boolean = true
+) {
+    var text by rememberSaveable { mutableStateOf(value) }
+    var showDatePickerModal by rememberSaveable { mutableStateOf(false) }
+
+    fun switchStateDatePickerModal() {
+        showDatePickerModal = !showDatePickerModal
+    }
+
+    OutlinedTextField(
+        value = text,
+        onValueChange = {},
+        textStyle = TextStyle(fontSize = 20.sp),
+        modifier = modifier
+            .width(200.dp)
+            .height(60.dp),
+        label = {
+            Text(
+                text = title,
+                fontSize = 20.sp
+            )
+        },
+        maxLines = 1,
+        enabled = isEnabled,
+        trailingIcon =
+        {
+            Icon(
+                imageVector = Icons.Filled.EditCalendar,
+                contentDescription = "Calendar",
+                modifier = Modifier.clickable { switchStateDatePickerModal() }
+            )
+        },
+        readOnly = true,
+    )
+
+    if (showDatePickerModal) {
+        DatePickerModal(
+            onDateSelected = { date -> text = date.toString(); onChangeDate(date.toString()) },
+            onDismissRequest = ::switchStateDatePickerModal
+        )
+    }
+}
