de85acbf257a1cb728e0fe28a02fcb0851561874
axelhertzberg <axelher@student.chalmers.se>
2022-04-27 11:34:23 +0200

    Fixed broken branch
    * Landing overview done

--- androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/data/ViewModelModule.kt
+++ androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/data/ViewModelModule.kt
@@ -1,7 +1,9 @@
 package com.EENX15_22_17.digital_journal.android.data
 
+import com.EENX15_22_17.digital_journal.android.screens.JournalEventViewModel
 import com.EENX15_22_17.digital_journal.android.screens.current.currentpatients.PatientListViewModel
 import org.kodein.di.DI
+import org.kodein.di.bindMultiton
 import org.kodein.di.bindSingleton
 import org.kodein.di.instance
 
@@ -9,4 +11,9 @@ val ViewModelModule = DI.Module("ViewModelModule") {
 
     bindSingleton { PatientListViewModel(instance()) }
 
+    bindMultiton<String, JournalEventViewModel> { journalId ->
+        JournalEventViewModel(journalRepository = instance(), journalId = journalId)
+    }
+
+    bindSingleton { JournalEventViewModel(instance(), "626803fd7f788d66486d6561") }
 }
