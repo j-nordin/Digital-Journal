bc6b3b87fdcaae319166932d7ee0376607e29a5c
j-nordin <57144825+j-nordin@users.noreply.github.com>
2022-05-19 23:51:43 +0200

    Retts color circle redesigned

--- androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/screens/journalList/JournalEntrySummaryCardListItem.kt
+++ androidApp/src/main/java/com/EENX15_22_17/digital_journal/android/screens/journalList/JournalEntrySummaryCardListItem.kt
@@ -7,137 +7,11 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import com.EENX15_22_17.digital_journal.android.ui.components.TitledSectionWithRettsColor
 import se.predicare.journal.data.JournalEntryDto
 
-/*@Composable
-// TODO: Replace default parameter-values with PreviewParameter-annotations when tooling is less buggy.
-fun JournalEntrySummaryCardListItem(
-    journalId: String = "1",
-    name: String = "PatientName",
-    securityNumber: String = "00000000",
-    onNavigateToJournal: (journalId: String) -> Unit = {},
-    onNavigateToPatient: (patientId: String) -> Unit = {}
-) {
-    //TODO: use navigation to show these
-    var showWarning by remember { mutableStateOf(false) }
-    var showComments by remember { mutableStateOf(false) }
-    Card(
-        modifier = Modifier
-            .padding(vertical = 2.dp)
-            .fillMaxWidth()
-            .clickable { onNavigateToJournal(journalId) },
-    ) {
-        Row(horizontalArrangement = Arrangement.SpaceBetween) {
-            Text(
-                text = name,
-                modifier = Modifier.padding(16.dp),
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Bold
-            )
-            Text(
-                text = securityNumber,
-                modifier = Modifier.padding(16.dp),
-                fontSize = 20.sp
-            )
-            Row(horizontalArrangement = Arrangement.End) {
-                // TODO: Navigate using patient id
-                IconButton(onClick = { onNavigateToPatient("<patientId goes here>") }) {
-                    Icon(
-                        imageVector = Icons.Rounded.Info,
-                        contentDescription = "Overview",
-                        tint = colorIcon
-                    )
-                }
-                IconButton(
-                    onClick = { showComments = true }) {
-                    Icon(
-                        imageVector = Icons.Rounded.Notifications,
-                        contentDescription = "Comments",
-                        tint = colorIcon
-                    )
-                }
-                IconButton(onClick = { showWarning = true }) {
-                    Icon(
-                        imageVector = Icons.Rounded.Warning,
-                        contentDescription = "Warnings",
-                        tint = danger
-                    )
-                }
-                if (showWarning) {
-                    Dialog(onDismissRequest = { showWarning = false }) {
-                        WarningContent(journalId = journalId)
-                    }
-                } else if (showComments) {
-                    Dialog(onDismissRequest = { showComments = false }) {
-                        CommentsContent(journalId = journalId)
-                    }
-                }
-            }
-        }
-    }
-}
-
-
-//TODO: Replace with the real warningContent
-@Composable
-fun WarningContent(journalId: String) {
-    //TODO: Get font sizes (and other theme-related stuff) from the theme
-    Surface(
-        modifier = Modifier
-            .width(300.dp)
-            .height(420.dp)
-            .padding(16.dp)
-    ) {
-        Column {
-            Text(
-                text = "This is a warning. The id is displayed below",
-                style = MaterialTheme.typography.h6
-            )
-            Text(
-                text = journalId,
-                modifier = Modifier
-                    .fillMaxHeight()
-                    .fillMaxWidth(),
-                textAlign = TextAlign.Center,
-                style = MaterialTheme.typography.h4
-            )
-        }
-    }
-}
-
-//TODO: Replace with the real commentContent
-@Composable
-fun CommentsContent(journalId: String) {
-    Surface(
-        modifier = Modifier
-            .width(300.dp)
-            .height(420.dp)
-            .padding(16.dp)
-    ) {
-        Column {
-            Text(
-                text = "This is a comment. The id is displayed below",
-                style = MaterialTheme.typography.h6
-            )
-            Text(
-                text = journalId,
-                modifier = Modifier
-                    .fillMaxHeight()
-                    .fillMaxWidth(),
-                textAlign = TextAlign.Center,
-                style = MaterialTheme.typography.h4
-            )
-        }
-    }
-}
-*/
-
-
-@Preview
 @Composable
 fun JournalList(
     modifier: Modifier = Modifier,
@@ -153,16 +27,7 @@ fun JournalList(
             .height(400.dp)
     ) {
         for (journal in journals) {
-/*            JournalEntrySummaryCardListItem(
-                journalId = journal._id ?: "",
-                name = journal.patientInfo.patientData?.name ?: "",
-                securityNumber = journal.patientInfo.patientData?.personalId ?: "",
-                // TODO: Navigate using real patientID
-                onNavigateToPatient = { onNavigateToPatient("<patientId goes here>") },
-                onNavigateToJournal = { onNavigateToJournal(it) }
-            )*/
-
-            JournalListSummaryCard(
+            JournalListCard(
                 /*modifier = Modifier.clickable { journal._id?.let { onNavigateToJournal(it) } },
                 title = journal.patientInfo.patientData?.name ?: "",*/
                 journal = journal,
@@ -175,7 +40,7 @@ fun JournalList(
 }
 
 @Composable
-fun JournalListSummaryCard(
+fun JournalListCard(
     journal: JournalEntryDto,
     onNavigateToPatient: () -> Unit,
     onNavigateToJournal: (journalId: String) -> Unit
